b364c2f0e63fd72112831a974e82644322237caa
antoine <antoine.jolivet29@gmail.com>
2024-02-29 12:14:30 +0100

    create autoSlug interface

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,10 +6,17 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
+
+    public function __construct(private FormListenerFactory $listenerFactory)
+    {
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -19,7 +26,8 @@ class UserType extends AbstractType
             ->add('shortDescription')
             ->add('longDescription')
             ->add('isOpenToWork')
-            ->add('save', SubmitType::class);
+            ->add('save', SubmitType::class)
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->listenerFactory->autoSlug('fullname'));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
